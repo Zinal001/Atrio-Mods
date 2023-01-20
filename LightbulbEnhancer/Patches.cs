f77f3f94dad550743b4ee946b6659f8ec319bfa4
Zinal001 <Zinal001@users.noreply.github.com>
2023-01-20 14:21:28 +0100

    v1.0.0

---
+++ LightbulbEnhancer/Patches.cs
@@ -0,0 +1,28 @@
+﻿using Isto.Atrio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LightbulbEnhancer
+{
+    static class Patches
+    {
+        [HarmonyLib.HarmonyPatch(typeof(LightSystemLight), "Awake")]
+        [HarmonyLib.HarmonyPostfix()]
+        private static void LightSystemLight_Awake_Postfix(Light ____light)
+        {
+            if(____light != null)
+                ____light.range *= Plugin.Instance.LightbulbRangeMultiplier.Value;
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(TetheredLight), "Awake")]
+        [HarmonyLib.HarmonyPostfix()]
+        private static void TetheredLight_Awake_Postfix(TetheredLight __instance, PowerTethered ____tether)
+        {
+            if (____tether != null)
+                ____tether.maxRange *= Plugin.Instance.LightbulbRangeMultiplier.Value;
+        }
+
+    }
+}
---
+++ LightbulbEnhancer/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+
+namespace LightbulbEnhancer
+{
+    [BepInPlugin("tech.zinals.atrio.lightbulbenhancer", "Lightbulb Enhancer", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance { get; private set; }
+
+        private HarmonyLib.Harmony _Harmony;
+
+        public BepInEx.Configuration.ConfigEntry<float> LightbulbRangeMultiplier;
+
+        public Plugin()
+        {
+            Instance = this;
+            LightbulbRangeMultiplier = Config.Bind("Light System", "Range Multiplier", 1f, "Multiplier for the range of each lightbulb");
+        }
+
+        private void Awake()
+        {
+            _Harmony = new HarmonyLib.Harmony("tech.zinals.atrio.torchenhancer");
+            _Harmony.PatchAll(typeof(Patches));
+        }
+    }
+}
